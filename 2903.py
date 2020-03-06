bf8049f25e1c98d56e447c9923fb4c29bde58c9f
Marbleling <ljiho1998@naver.com>
2020-03-06 19:22:18 +0900

    Problem 2903 Solved

---
+++ 2903.py
@@ -0,0 +1,10 @@
+# project : baekjoon python algorithm practice
+# user : marble
+# date : 20. 3. 6.
+# time : 오후 7:13
+# problem : https://www.acmicpc.net/problem/2903
+# problem type : Math
+
+# 일반식 잘 세워서 풀면 풀리는 쉬운 문제
+n = int(input())
+print((1+pow(2, n)) ** 2)
