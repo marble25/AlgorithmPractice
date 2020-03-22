10f81178083e991fedf03160fad59b43c4d6c598
Marbleling <ljiho1998@naver.com>
2020-03-22 15:23:06 +0900

    Problem 1000 Solved

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a, b;
+    cin >> a >> b;
+    cout << a+b << endl;
     return 0;
 }
