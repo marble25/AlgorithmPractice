8f9ace494016531be9fb8c082f254b85d60b7fe7
Marbleling <ljiho1998@naver.com>
2020-03-23 18:48:25 +0900

    Problem 12015 Solved

--- 12015.cpp
+++ 12015.cpp
@@ -1,3 +1,7 @@
+//
+// Created by marble on 20. 3. 23..
+//
+
 #include <iostream>
 #include <cmath>
 #include <algorithm>
@@ -23,7 +27,7 @@ int main() {
         if (temp > v.back()) { // 벡터에 있는 가장 큰 값보다 크면
             v.push_back(temp); // 그냥 넣고
             cnt ++; // LIS 길이 + 1해줌
-        } else {
+        } else { // vector 안의 값을 바꿔주는 이유는 10, 20, 10, 15, 18이 들어온다고 쳤을 때 20을 15로 바꿔줌으로 18이 추기될 수 있기 때문. 더 긴 증가 수열을 만들 수 있는 여지를 준다.
             auto low = lower_bound(v.begin(), v.end(), temp);
             *low = temp; // temp보다 약간 큰 값을 temp로 바꿔줌
         }
