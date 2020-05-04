cf17fff04e4b45ed957108b71c3ddeef24f1458a
Marbleling <ljiho1998@naver.com>
2020-05-04 13:48:18 +0900

    Past Solved Problem Updated

--- 1000.cpp
+++ 1000.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-
-    int a, b;
-    cin >> a >> b;
-    cout << a+b << endl;
-    return 0;
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
 }
