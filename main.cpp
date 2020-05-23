7c500fb041884689ae42c5d3b5e6945dfe4a08fc
Marbleling <ljiho1998@naver.com>
2020-05-23 23:40:43 +0900

    Problem Solving

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-// Stack을 이용한 간단한 문제
-
 #include <iostream>
 #include <cmath>
 #include <algorithm>
@@ -13,8 +11,7 @@
 using namespace std;
 
 int n;
-char word[100005];
-int cnt = 0;
+int table[1505][1505];
 
 int main() {
     cin.tie(NULL);
@@ -23,34 +20,12 @@ int main() {
 
     cin >> n;
     for(int i=0;i<n;i++) {
-        cin >> word;
-
-        int len = strlen(word);
-        stack<int> st;
-
-        for(int j=0;j<len;j++) {
-            if(word[j] == 'A') { // A가 들어오면
-                if(st.empty() || st.top() != 1) { // Stack에 A가 없다면 push
-                    st.push(1);
-                } else { // A가 있으면 A를 빼줌
-                    st.pop();
-                }
-
-            } else if(word[j] == 'B'){ // B가 들어오면
-                if(st.empty() || st.top() != 2) { // Stack에 B가 없다면 push
-                    st.push(2);
-                } else { // B가 있으면 B를 빼줌
-                    st.pop();
-                }
-            }
-        }
-
-        if(st.empty()) { // Stack이 비어있으면(Stack의 쌍이 모두 맞아 떨어지면) cnt 증가
-            cnt++;
+        for(int j=0;j<n;j++) {
+            cin >> table[i][j];
         }
     }
 
-    cout << cnt;
+
 
     return 0;
 }
