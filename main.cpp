a4f6172dbd8fb478c7f3a2799b9fe06a3b74944b
Marbleling <ljiho1998@naver.com>
2020-06-11 23:50:32 +0900

    Problem Solving

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// 간단하게 맞춘 문제
+
 
 #include <iostream>
 #include <cmath>
@@ -12,61 +12,54 @@
 #include <string.h>
 using namespace std;
 
-int n, m;
-int cnt = 0;
-int arr[10];
-int temp[10] = {0};
-int re[10005][10] = {0};
-
-
-void perm(int stage, int idx) { // 모든 경우를 permutation함
-    if(stage == m-1) { // 끝에 도달했다면
-        int equal_count = 0;
-        for(int i=0;i<cnt;i++) {
-            equal_count = 0;
-            for(int j=0;j<m;j++) {
-                if(temp[j] == re[i][j]) {
-                    equal_count++;
-                }
-            }
-
-            if(equal_count == m) { // 같은 답이 존재한다면
-                break;
-            }
-        }
-
-        if(equal_count != m) { // 같은 답이 존재하지 않으면 배열에 추가
-            for(int i=0;i<m;i++) {
-                re[cnt][i] = temp[i];
-            }
-            cnt++;
-        }
-        return;
-    }
-
-    for(int i=idx+1;i<n;i++) { // 모든 경우를 permute
-        temp[stage+1] = arr[i];
-        perm(stage+1, i);
-        temp[stage+1] = 0;
-    }
-}
+int n, a, b;
+int pos[500005] = {0};
+int temp[500005] = {0};
 
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> n >> m;
-    for(int i=0;i<n;i++) {
-        cin >> arr[i];
-    }
-    sort(arr, arr+n); // 오름차순 정렬
-    perm(-1, -1);
-    for(int i=0;i<cnt;i++) {
-        for(int j=0;j<m;j++) {
-            cout << re[i][j] << " ";
+    cin >> n >> a >> b;
+    pos[a] = 1;
+    pos[b] = 2;
+
+    for(int i=1;;i++) {
+        bool is_reachable = false;
+
+        int diff = pow(2, i-1);
+
+        for(int j=1;j<=n;j++) {
+            if(pos[j] != 0) {
+                is_reachable = true;
+
+                int duck = pos[j];
+                if(j-diff > 0) {
+                    if(temp[j-diff] == 3-duck) {
+                        cout << i;
+                        return 0;
+                    }
+                    temp[j-diff] = duck;
+                }
+
+                if(j+diff <= n) {
+                    if(temp[j+diff] == 3-duck) {
+                        cout << i;
+                        return 0;
+                    }
+                    temp[j+diff] = duck;
+                }
+            }
         }
-        cout << "\n";
+
+        if(!is_reachable) {
+            cout << -1;
+            break;
+        }
+
+        memcpy(pos, temp, sizeof(pos));
+        memset(temp, 0, sizeof(temp));
     }
 
     return 0;
