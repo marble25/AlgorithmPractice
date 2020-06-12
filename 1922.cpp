137d06eb7bc94444037ccd6159a0b6fa0e1f7fc9
Marbleling <ljiho1998@naver.com>
2020-06-13 02:03:25 +0900

    Problem Solved(Using Union-Find, Kruskal, MST Algorithm)

--- 1922.cpp
+++ 1922.cpp
@@ -1,3 +1,9 @@
+//
+// Created by marble on 20. 6. 13..
+//
+
+// 전에는 쉽지 않았을 텐데 크루스칼 알고리즘을 잘 익혀서 쉽게 풀었던 문제
+
 #include <iostream>
 #include <cmath>
 #include <algorithm>
@@ -17,19 +23,19 @@ int rnk[1005] = {0};
 int p[1005] = {0};
 int re = 0;
 
-void init() {
+void init() { // 자신의 부모를 자기 자신으로 설정하고 rnk도 1로 설정해줌
     for(int i=1;i<=n;i++) {
         rnk[i] = 1;
         p[i] = i;
     }
 }
 
-int get_root(int x) {
+int get_root(int x) { // root를 찾음
     int y = p[x];
     while(y != p[y]) {
         y = p[y];
     }
-    p[x] = y;
+    p[x] = y; // x의 parent를 y로 다이렉트로 연결
     return y;
 }
 
@@ -37,16 +43,16 @@ bool merge(int x, int y) {
     x = get_root(x);
     y = get_root(y);
 
-    if(x == y) {
+    if(x == y) { // x와 y의 부모가 같으면 이미 같은 그룹에 있으므로 더이상 처리 안함
         return false;
     } else {
-        if(rnk[x] < rnk[y]) {
+        if(rnk[x] < rnk[y]) { // 랭크가 더 작은 쪽이 큰 쪽의 아래로 들어감
             p[x] = y;
         } else {
             p[y] = x;
         }
 
-        if(rnk[x] == rnk[y]) {
+        if(rnk[x] == rnk[y]) { // 랭크가 같으면 루트의 랭크를 증가
             rnk[x]++;
         }
 
