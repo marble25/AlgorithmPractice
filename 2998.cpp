177b18849e81f6b6e168f83f1d412642c1ced58a
Marbleling <ljiho1998@naver.com>
2020-07-06 00:26:14 +0900

    Problem Solved

--- 2998.cpp
+++ 2998.cpp
@@ -1,4 +1,6 @@
-// 간단한 문제
+//
+// Created by marble on 20. 7. 6..
+//
 
 #include <iostream>
 #include <cmath>
@@ -23,18 +25,18 @@ int main() {
 
     int sz = strlen(s);
 
-    int num = 0; // 처음 값을 설정
+    int num = 0;
     if(sz % 3 == 1) {
         num = s[0] - '0';
     } else if(sz % 3 == 2) {
         num = (s[0] - '0') * 2 + (s[1] - '0');
     }
 
-    if(num != 0) { // 처음 값이 있으면 출력
+    if(num != 0) {
         cout << num;
     }
 
-    for(int i=sz%3;i<sz;i+=3) { // 3개 단위로 끊어서 출력
+    for(int i=sz%3;i<sz;i+=3) {
         num = (s[i] - '0') * 4 + (s[i+1] - '0') * 2 + (s[i+2] - '0');
         cout << num;
     }
