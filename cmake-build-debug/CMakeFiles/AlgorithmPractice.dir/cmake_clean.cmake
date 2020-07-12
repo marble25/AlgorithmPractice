3af61fe53fed23715fc41c627628b416d1b8ade3
Marbleling <ljiho1998@naver.com>
2020-07-12 23:06:39 +0900

    initial commit

---
+++ cmake-build-debug/CMakeFiles/AlgorithmPractice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AlgorithmPractice"
+  "AlgorithmPractice.pdb"
+  "CMakeFiles/AlgorithmPractice.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AlgorithmPractice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
