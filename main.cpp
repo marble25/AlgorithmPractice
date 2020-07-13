ea25902682cda6ded7a9a6e272ab0f6d1a7b2e3a
Marbleling <ljiho1998@naver.com>
2020-07-13 23:50:56 +0900

    Problem Solving

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-// 간단한 문제
-
 #include <iostream>
 #include <cmath>
 #include <algorithm>
@@ -12,32 +10,30 @@
 #include <string.h>
 using namespace std;
 
-char s[105];
+int n;
+int s[20][20] = {0};
+
+int cache[1<<20] = {0};
+bool visited[1<<20] = {0};
+
+int f(int x) {
+    for(int i=x+1;i<n;i++) {
+
+    }
+}
 
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> s;
-
-    int sz = strlen(s);
-
-    int num = 0; // 처음 값을 설정
-    if(sz % 3 == 1) {
-        num = s[0] - '0';
-    } else if(sz % 3 == 2) {
-        num = (s[0] - '0') * 2 + (s[1] - '0');
+    cin >> n;
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+            cin >> s[i][j];
+        }
     }
 
-    if(num != 0) { // 처음 값이 있으면 출력
-        cout << num;
-    }
-
-    for(int i=sz%3;i<sz;i+=3) { // 3개 단위로 끊어서 출력
-        num = (s[i] - '0') * 4 + (s[i+1] - '0') * 2 + (s[i+2] - '0');
-        cout << num;
-    }
 
     return 0;
 }
