24e13c1fc450a1e0cb4df732b7f4a56934494446
Marbleling <ljiho1998@naver.com>
2020-07-28 00:00:58 +0900

    Problem Solved(Using BFS)

--- main.cpp
+++ main.cpp
@@ -15,21 +15,34 @@ int arr[505][505] = {0};
 int visited[505][505] = {0};
 queue<pair<int, int>> q;
 
+int ans = 0;
+int mx = 0;
+
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
 void bfs(int x, int y) {
     q.push({x, y});
     visited[x][y] = 1;
+    int cnt = 1;
 
     while(!q.empty()) {
         x=q.front().first, y=q.front().second;
         q.pop();
 
         for(int i=0;i<4;i++) {
-            if()
+            int cx=x+dx[i], cy=y+dy[i];
+            if(cx < 1 || cy < 1 || cx > n || cy > m) continue;
+            if(arr[cx][cy] == 0) continue;
+            if(visited[cx][cy] == 1) continue;
+            visited[cx][cy] = 1;
+            q.push({cx, cy});
+            cnt++;
         }
     }
+
+    ans++;
+    mx = max(mx, cnt);
 }
 
 int main() {
@@ -51,6 +64,8 @@ int main() {
             }
         }
     }
+
+    cout << ans << "\n" << mx;
 
     return 0;
 }
