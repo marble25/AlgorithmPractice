fab26d9ed6c6451c9eeeea33d48d700e0e0b9311
Marbleling <ljiho1998@naver.com>
2020-08-01 11:22:59 +0900

    Problem Solved(Using Greedy and DFS)

--- 1135.cpp
+++ 1135.cpp
@@ -1,3 +1,10 @@
+//
+// Created by marble on 20. 8. 1..
+//
+
+// 쉽사리 이해가 가지 않는 문제이다.
+// 그리디 전략을 어떻게 짜야 할지 감이 잘 안왔다.
+
 #include <iostream>
 #include <cmath>
 #include <algorithm>
@@ -20,14 +27,14 @@ bool comp(const pair<int, int> a, const pair<int, int> b) {
 
 int dfs(int here) {
     int ret = 0;
-    for(int i=0;i<vc[here].size();i++) {
+    for(int i=0;i<vc[here].size();i++) { // 자기 아래 노드들의 비용을 모두 구함
         int next = vc[here][i].second;
         vc[here][i].first = 1 + dfs(next);
     }
 
-    sort(vc[here].begin(), vc[here].end(), comp);
+    sort(vc[here].begin(), vc[here].end(), comp); // 비용이 큰 순서대로 정렬
 
-    for(int i = 0;i<vc[here].size();i++) {
+    for(int i = 0;i<vc[here].size();i++) { // 최대 비용을 구함
         vc[here][i].first += i;
         ret = max(ret, vc[here][i].first);
     }
