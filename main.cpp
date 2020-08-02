a3a1c6082892f81706596ab5966f441578cfcdd0
Marbleling <ljiho1998@naver.com>
2020-08-02 22:33:45 +0900

    Problem Solved(Using Greedy and Sorting)

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+// 단순 정렬하는 문제였다.
+// 어려운 문제도 꾸준히 풀어야 겠다.
+
 #include <iostream>
 #include <cmath>
 #include <algorithm>
@@ -12,45 +15,28 @@
 using namespace std;
 
 int n;
-vector<pair<int, int>> vc[55];
-
-bool comp(const pair<int, int> a, const pair<int, int> b) {
-    return a.first > b.first;
-}
-
-int dfs(int here) {
-    int ret = 0;
-    for(int i=0;i<vc[here].size();i++) {
-        int next = vc[here][i].second;
-        vc[here][i].first = 1 + dfs(next);
-    }
-
-    sort(vc[here].begin(), vc[here].end(), comp);
-
-    for(int i = 0;i<vc[here].size();i++) {
-        vc[here][i].first += i;
-        ret = max(ret, vc[here][i].first);
-    }
-
-    return ret;
-}
+vector<int> v;
 
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
+    int ans = 0;
+
     cin >> n;
     for(int i=0;i<n;i++) {
-        int val;
-        cin >> val;
-        if(val != -1) {
-            vc[val].push_back({0, i});
-        }
+        int x;
+        cin >> x;
+        v.push_back(x);
     }
 
+    sort(v.begin(), v.end(), greater<>()); // 가장 오래 걸리는 것을 먼저 심음
+    for(int i=0;i<n;i++) {
+        ans = max(ans, v[i]+i+1); // 심은 날짜를 고려했을 때 가장 늦게 끝나는 것 고르기
+    }
 
-    cout << dfs(0);
+    cout << ans + 1;
 
     return 0;
 }
