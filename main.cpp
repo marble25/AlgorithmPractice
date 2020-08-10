ad645161988e631ab352b92c984f76c2dbd502da
Marbleling <ljiho1998@naver.com>
2020-08-10 23:28:22 +0900

    Problem Solving(Using Two Pointer)

--- main.cpp
+++ main.cpp
@@ -11,53 +11,34 @@
 #include <string.h>
 using namespace std;
 
-int n;
-int t[505] = {0};
-vector<int> linked[505];
-int order[505] = {0};
-int visited[505] = {0};
+int n, s;
+int arr[100005];
 
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> n;
-    for(int i=1;i<=n;i++) {
-        cin >> t[i];
-        while(true) {
-            int x;
-            cin >> x;
-            if(x == -1) break;
-            linked[x].push_back(i);
-            order[i]++;
+    cin >> n >> s;
+
+    for(int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+
+    int st = 0, en = -1;
+    int sum = 0;
+    int ans = 9999999;
+    while(en < n) {
+        if(sum < s) {
+            en++;
+            if(en == n) break;
+            sum += arr[en];
+        } else {
+            ans = min(ans, en-st+1);
+            sum -= arr[st++];
         }
     }
-
-    queue<int> q;
-
-    for(int i=1;i<=n;i++) {
-        if(order[i] == 0) q.push(i);
-    }
-
-    while(!q.empty()) {
-        int x = q.front();
-        q.pop();
-
-        for(int i=0;i<linked[x].size();i++) {
-            int it=linked[x][i];
-            order[it]--;
-            visited[it] = max(visited[it], visited[x] + t[x]);
-            if(order[it] == 0) {
-                q.push(it);
-
-            }
-        }
-    }
-
-    for(int i=1;i<=n;i++) {
-        cout << visited[i] + t[i] << '\n';
-    }
-
+    if(ans == 9999999) cout << 0;
+    else cout << ans;
     return 0;
 }
