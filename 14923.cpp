f15eb91a59e7e2a26ce6f9727cb781363ffbbd97
Marbleling <ljiho1998@naver.com>
2020-08-13 15:46:14 +0900

    Problem Solved(Using DP & BFS)

--- 14923.cpp
+++ 14923.cpp
@@ -1,3 +1,10 @@
+//
+// Created by marble on 20. 8. 13..
+//
+
+// 시간이 생각보다 많이 나와서 아쉬운 문제
+// 시간을 더 줄일 수 있는 방법을 생각해 봐야겠다
+
 #include <iostream>
 #include <cmath>
 #include <algorithm>
@@ -15,7 +22,7 @@ int n, m;
 int hx, hy;
 int ex, ey;
 int arr[1005][1005] = {0};
-int visited[2][1005][1005] = {0};
+int visited[2][1005][1005] = {0}; // 0번 부섰을 때 그 포인트를 방문했는지와 1번 부섰을 때 그 포인트를 방문했는지 체크하는 배열
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
@@ -38,16 +45,16 @@ void bfs() {
         for(int i=0;i<4;i++) {
             int cx = x + dx[i], cy = y + dy[i];
             if(cx < 1 || cy < 1 || cx > n || cy > m) continue;
-            if(cnt == 1 && arr[cx][cy] == 1) continue;
-            if(cnt == 1 && visited[cnt][cx][cy] != 0) continue;
-            if(cnt == 0 && arr[cx][cy] == 0 && visited[cnt][cx][cy] != 0) continue;
-            if(cnt == 0 && arr[cx][cy] == 1 && visited[cnt+1][cx][cy] != 0) continue;
+            if(cnt == 1 && arr[cx][cy] == 1) continue; // 이미 1번 부섰는데 또 벽이 있으면
+            if(cnt == 1 && visited[cnt][cx][cy] != 0) continue; // 이미 1번 부섰고, 그 포인트를 이미 방문했다면
+            if(cnt == 0 && arr[cx][cy] == 0 && visited[cnt][cx][cy] != 0) continue; // 1번도 안부섰고, 벽이 아닌 그 포인트를 방문했다면
+            if(cnt == 0 && arr[cx][cy] == 1 && visited[cnt+1][cx][cy] != 0) continue; // 1번도 안부섰고, 벽인 그 포인트를 방문했다면
 
-            if(arr[cx][cy] == 1) {
-                visited[cnt+1][cx][cy] = visited[cnt][x][y] + 1;
+            if(arr[cx][cy] == 1) { // 벽이면
+                visited[cnt+1][cx][cy] = visited[cnt][x][y] + 1; // 부숴보기
                 q.push({cnt+1, {cx, cy}});
-            } else {
-                visited[cnt][cx][cy] = visited[cnt][x][y] + 1;
+            } else { // 벽 아니면
+                visited[cnt][cx][cy] = visited[cnt][x][y] + 1; // 그대로 진행
                 q.push({cnt, {cx, cy}});
             }
         }
