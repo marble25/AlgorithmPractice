caa7b96e79a2a1c0de801dbb880eb342550c2481
Marbleling <ljiho1998@naver.com>
2020-08-14 23:39:23 +0900

    Problem Solving(Using Line Sweeping)

--- main.cpp
+++ main.cpp
@@ -11,51 +11,9 @@
 #include <string.h>
 using namespace std;
 
-int n, m;
-int hx, hy;
-int ex, ey;
-int arr[1005][1005] = {0};
-int visited[2][1005][1005] = {0};
-
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
-
-void bfs() {
-    queue<pair<int, pair<int, int>>> q;
-    q.push({0, {hx, hy}});
-    visited[0][hx][hy] = 1;
-
-    while(!q.empty()) {
-        int cnt = q.front().first;
-        int x = q.front().second.first, y = q.front().second.second;
-        q.pop();
-
-        if(x == ex && y == ey) {
-            cout << visited[cnt][x][y]-1;
-            return;
-        }
-
-        for(int i=0;i<4;i++) {
-            int cx = x + dx[i], cy = y + dy[i];
-            if(cx < 1 || cy < 1 || cx > n || cy > m) continue;
-            if(cnt == 1 && arr[cx][cy] == 1) continue;
-            if(cnt == 1 && visited[cnt][cx][cy] != 0) continue;
-            if(cnt == 0 && arr[cx][cy] == 0 && visited[cnt][cx][cy] != 0) continue;
-            if(cnt == 0 && arr[cx][cy] == 1 && visited[cnt+1][cx][cy] != 0) continue;
-
-            if(arr[cx][cy] == 1) {
-                visited[cnt+1][cx][cy] = visited[cnt][x][y] + 1;
-                q.push({cnt+1, {cx, cy}});
-            } else {
-                visited[cnt][cx][cy] = visited[cnt][x][y] + 1;
-                q.push({cnt, {cx, cy}});
-            }
-        }
-    }
-
-    cout << -1;
-
-}
+long long n, m;
+long long mn = 100000005, mx = -1;
+vector<pair<long long, long long>> people;
 
 int main() {
     cin.tie(NULL);
@@ -63,15 +21,34 @@ int main() {
     ios_base::sync_with_stdio(false);
 
     cin >> n >> m;
-    cin >> hx >> hy;
-    cin >> ex >> ey;
-    for(int i=1;i<=n;i++) {
-        for(int j=1;j<=m;j++) {
-            cin >> arr[i][j];
+    for(int i=0;i<n;i++) {
+        long long x, y;
+        cin >> x >> y;
+        if(x > y) {
+            people.push_back({x, y});
         }
     }
+    if(people.empty()) {
+        cout << m;
+        return 0;
+    }
 
-    bfs();
+    sort(people.begin(), people.end(), greater<>());
+
+    long long total = 0;
+    long long len = people[0].second - people[0].first;
+    long long start = people[0].first;
+
+    int sz = people.size();
+    for(int i=1;i<sz;i++) {
+        if(people[i].first < start - len) {
+            total += len;
+            start = people[i].first;
+        }
+        if(people[i].second < start - len) {
+            len = start - people[i].second;
+        }
+    }
 
     return 0;
 }
