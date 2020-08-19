6bdf515fc5c052fa63fee7862710d685238f99ab
Marbleling <ljiho1998@naver.com>
2020-08-19 23:56:53 +0900

    Problem Solving(Using DFS)

--- main.cpp
+++ main.cpp
@@ -11,29 +11,21 @@
 #include <string.h>
 using namespace std;
 
-int x, y, d, t;
+int n, s, d;
+vector<int> linked[100005];
 
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> x >> y >> d >> t;
-
-    double dis = sqrt(x * x + y * y);
-    int jump = dis / d;
-    double spare = dis - jump * d;
-    double result = dis;
-    result = min(result, spare + jump * t);
-    if(jump > 0) {
-        result = min(result, (jump+1)*t*1.0);
-    } else {
-        result = min(result, d-spare+t);
-        if(dis<d) result = min(result, t*2.0);
+    cin >> n >> s >> d;
+    for(int i=0;i<n;i++) {
+        int x, y;
+        cin >> x >> y;
+        linked[x].push_back(y);
+        linked[y].push_back(x);
     }
-    cout << fixed;
-    cout.precision(20);
-    cout << result;
 
 
 
