dfbafd371f38bc04de3bb1934b771774913cce2a
marble25 <ljiho1998@naver.com>
2020-10-07 23:45:50 +0900

    Problem Solved(Using Simulation)

--- 3190.cpp
+++ 3190.cpp
@@ -1,4 +1,6 @@
-// 간단한 문제지만 여러번 조건을 따져줘야 하는 문제
+//
+// Created by marble on 20. 10. 7..
+//
 
 #include <iostream>
 #include <cmath>
@@ -47,7 +49,6 @@ int main() {
         change[x] = c;
     }
 
-    // 시작 포인트를 (1, 1)로 초기화
     head_x = 1;
     head_y = 1;
 
@@ -71,22 +72,18 @@ int main() {
                 break;
         }
 
-        // 끝에 도달했거나 자기 자신과 만나면 끝
         if(head_x < 1 || head_y < 1 || head_x > n || head_y > n) break;
         if(board[head_x][head_y] == SNAKE) break;
 
-        // 사과가 아닌 경우에는 queue의 마지막을 빼고, 이 좌표를 0으로 채워줌
         if(board[head_x][head_y] != APPLE) {
             int x = q.front().first, y = q.front().second;
             board[x][y] = 0;
             q.pop();
         }
 
-        // 새로 이동한 부분을 queue에 넣음
         board[head_x][head_y] = SNAKE;
         q.push({head_x, head_y});
 
-        // 방향 전환
         if(change[time] == 'L') {
             dir = (dir-1+4)%4;
         } else if(change[time] == 'D') {
