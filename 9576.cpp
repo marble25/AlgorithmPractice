9cc11015159bc66030b219febe61f103e7c509b3
marble <ljiho1998@naver.com>
2020-11-23 19:03:02 +0900

    Problem Solved(Using Greedy)

--- 9576.cpp
+++ 9576.cpp
@@ -1,5 +1,6 @@
-// break문 하나로 정답이 갈린 문제
-// 항상 꼼꼼하게...
+//
+// Created by marble on 20. 11. 23..
+//
 
 #include <iostream>
 #include <cmath>
@@ -35,13 +36,13 @@ int main() {
             v.push_back({y, x});
         }
 
-        sort(v.begin(), v.end()); // 뒤의 범위를 기준으로 정렬
+        sort(v.begin(), v.end());
         int cnt = 0;
 
         for(int i=0;i<m;i++) {
             int start = v[i].second, end = v[i].first;
             for(int j=start;j<=end;j++) {
-                if(checked[j] == 0) { // 비어있으면 채워넣기
+                if(checked[j] == 0) {
                     cnt++;
                     checked[j] = 1;
                     break;
