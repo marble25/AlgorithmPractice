417f15a56fa802ee501f925f78ab2046fd9bd512
marble <ljiho1998@naver.com>
2020-11-26 11:47:12 +0900

    Problem Solved(Using DP & Dijkstra)

--- 1162.cpp
+++ 1162.cpp
@@ -18,10 +18,9 @@
 using namespace std;
 
 int n, m, k;
-vector<pair<long long int, long long int>> v[10005];
-priority_queue<pair<long long int, pair<long long int, long long int>>> pq;
-bool visited[10005][25] = {false};
-long long int dis[10005][25] = {0};
+vector<pair<long long, long long>> v[10005];
+priority_queue<pair<long long, pair<long long, long long>>> pq;
+long long dis[10005][25] = {0};
 
 int main() {
     cin.tie(NULL);
@@ -30,39 +29,38 @@ int main() {
 
     cin >> n >> m >> k;
     for(int i=0;i<m;i++) {
-        long long int x, y, z;
+        long long x, y, z;
         cin >> x >> y >> z;
         v[x].push_back({y, z});
         v[y].push_back({x, z});
     }
 
-    visited[1][0] = true;
+    memset(dis, 0x3f, sizeof(dis));
+
     dis[1][0] = 0;
     pq.push({0, {0, 1}});
 
     while(!pq.empty()) {
-        long long int covered = pq.top().second.first, x = pq.top().second.second;
+        long long cost = -pq.top().first;
+        long long covered = pq.top().second.first, x = pq.top().second.second;
         pq.pop();
 
+        if(cost > dis[x][covered]) continue; // 이 부분이 시간초과를 없애주었다.
         for(int i=0;i<v[x].size();i++) {
             int next = v[x][i].first, next_cost = v[x][i].second;
-            if(!visited[next][covered] || (visited[next][covered] && dis[next][covered] > dis[x][covered] + next_cost)) {
-                visited[next][covered] = true;
+            if(dis[next][covered] > dis[x][covered] + next_cost) {
                 dis[next][covered] = dis[x][covered] + next_cost;
                 pq.push({-dis[next][covered], {covered, next}});
             }
-            if(covered+1 > k) continue;
-            if(!visited[next][covered+1] || (visited[next][covered+1] && dis[next][covered+1] > dis[x][covered])) {
-                visited[next][covered+1] = true;
+            if(covered < k && dis[next][covered+1] > dis[x][covered]) {
                 dis[next][covered+1] = dis[x][covered];
                 pq.push({-dis[next][covered+1], {covered+1, next}});
             }
         }
     }
 
-    long long int mn = INT64_MAX;
+    long long mn = INT64_MAX;
     for(int i=0;i<=k;i++) {
-        if(!visited[n][i]) continue;
         mn = min(mn, dis[n][i]);
     }
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 //
-// Created by marble on 20. 5. 30..
+// Created by marble on 20. 5. 2..
 //
 
-// 한 문장을 빠뜨려서 계속 틀렸던 문제
-// 알고리즘 검증을 똑바로 해야 한다
+// 잔실수를 너무 많이 한 문제
+// 잔실수를 줄여야 한다.
+// 문제 자체는 다익스트라 + dp로 쉽게 풀렸다.
 
 #include <iostream>
 #include <cmath>
@@ -12,93 +13,58 @@
 #include <stack>
 #include <vector>
 #include <queue>
-#include <map>
 #include <string>
 #include <string.h>
 using namespace std;
 
-int t;
-int f;
-map<string, int> mp;
-int parent[200005];
-int rk[200005];
-int sz[200005];
-
-int find(int x) { // 조상을 찾는 함수
-    int y = x;
-    while(parent[y] != y) { // 부모가 자기 자신일 때까지(조상) 올라감
-        y = parent[y];
-    }
-    parent[x] = y; // 현재 노드의 부모를 조상으로 설정
-    return y;
-}
-
-int merge(int x, int y) {
-    int ret = 0;
-    x = find(x);
-    y = find(y);
-    if(x == y) return sz[x]; // 같은 조상이면 merge 진행 안함
-    if(rk[x] < rk[y]) { // x의 차수가 y의 차수보다 작으면 x가 y쪽으로 붙음
-        parent[x] = y;
-        sz[y] += sz[x]; // y의 사이즈에 x 사이즈 더함
-        ret = sz[y];
-    } else { // 아니면
-        parent[y] = x;
-        sz[x] += sz[y]; // x 사이즈에 y 사이즈 더함
-        ret = sz[x];
-    }
-
-    if(rk[x] == rk[y]) { // 랭크가 같으면 랭크가 하나 증가(이때 root는 x)
-        rk[x]++;
-    }
-
-    return ret;
-}
+int n, m, k;
+vector<pair<long long, long long>> v[10005];
+priority_queue<pair<long long, pair<long long, long long>>> pq;
+long long dis[10005][25] = {0};
 
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> t;
-    while(t--) {
-        string s1, s2;
-        int k=1;
-        cin >> f;
+    cin >> n >> m >> k;
+    for(int i=0;i<m;i++) {
+        long long x, y, z;
+        cin >> x >> y >> z;
+        v[x].push_back({y, z});
+        v[y].push_back({x, z});
+    }
 
-        mp.clear();
-        memset(rk, 0, sizeof(rk));
-        memset(parent, 0, sizeof(parent));
-        memset(sz, 0, sizeof(sz));
+    memset(dis, 0x3f, sizeof(dis));
 
-        for(int i=0;i<f;i++) {
-            cin >> s1 >> s2;
+    dis[1][0] = 0;
+    pq.push({0, {0, 1}});
 
-            int n1, n2;
-            if(mp[s1] == 0) { // 맵에 값이 없으면
-                n1 = k++;
-                parent[n1] = n1; // 이 노드의 부모는 자기 자신
-                rk[n1] = 0; // 차수 0
-                sz[n1] = 1; // 사이즈 1
-                mp[s1] = n1; // 맵에 할당
-            } else {
-                n1 = mp[s1];
+    while(!pq.empty()) {
+        long long cost = -pq.top().first;
+        long long covered = pq.top().second.first, x = pq.top().second.second;
+        pq.pop();
+
+        if(cost > dis[x][covered]) continue; // 이 부분이 시간초과를 없애주었다.
+        for(int i=0;i<v[x].size();i++) {
+            int next = v[x][i].first, next_cost = v[x][i].second;
+            if(dis[next][covered] > dis[x][covered] + next_cost) {
+                dis[next][covered] = dis[x][covered] + next_cost;
+                pq.push({-dis[next][covered], {covered, next}});
             }
-
-            if(mp[s2] == 0) { // 맵에 값이 없으면
-                n2 = k++;
-                parent[n2] = n2; // 이 노드의 부모는 자기 자신
-                rk[n2] = 0; // 차수 0
-                sz[n2] = 1; // 사이즈 1
-                mp[s2] = n2; // 맵에 할당
-            } else {
-                n2 = mp[s2];
+            if(covered < k && dis[next][covered+1] > dis[x][covered]) {
+                dis[next][covered+1] = dis[x][covered];
+                pq.push({-dis[next][covered+1], {covered+1, next}});
             }
-
-            cout << merge(n1, n2) << "\n"; // n1, n2 합친 사이즈 출력
-
         }
     }
+
+    long long mn = INT64_MAX;
+    for(int i=0;i<=k;i++) {
+        mn = min(mn, dis[n][i]);
+    }
+
+    cout << mn;
 
     return 0;
 }
