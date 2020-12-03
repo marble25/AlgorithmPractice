f5431791747a2c7bae32021c75f9e92778a37619
marble <ljiho1998@naver.com>
2020-12-03 17:32:55 +0900

    Problem Solving(Using BFS)

--- main.cpp
+++ main.cpp
@@ -9,80 +9,63 @@
 #include <string.h>
 using namespace std;
 
-typedef struct Point {
-    int num;
-    int x;
-    int y;
-    int z;
-} point;
+int n, m, p;
+int s[1005] = {0};
+char in[1005];
 
-typedef struct Edge {
-    point p1;
-    point p2;
-    int weight;
-} edge;
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
 
-int n;
-vector<point> p;
-vector<edge> e;
+int mp[1005][1005] = {0};
 
-int rnk[100001] = {0};
-int parent[100001] = {0};
+int visited[1005][1005] = {0};
+queue<pair<int, int>> territory[1005];
 
-int ans = 0;
-int cnt = 0;
 
-bool compDis(edge e1, edge e2) {
-    return e1.weight < e2.weight;
+bool isEnd() {
+    bool flag = true;
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            if(mp[i][j] == 0) {
+                flag = false;
+            } else {
+                visited[i][j] = 1;
+            }
+        }
+    }
+    return flag;
 }
 
-bool compX(point p1, point p2) {
-    return p1.x < p2.x;
-}
+void printCastle() {
+    int castle[1005] = {0};
 
-bool compY(point p1, point p2) {
-    return p1.y < p2.y;
-}
-
-bool compZ(point p1, point p2) {
-    return p1.z < p2.z;
-}
-
-int distance(point p1, point p2) {
-    int x_dis = abs(p1.x - p2.x);
-    int y_dis = abs(p1.y - p2.y);
-    int z_dis = abs(p1.z - p2.z);
-
-    return min(x_dis, min(y_dis, z_dis));
-}
-
-int find(int node) {
-    int cur = parent[node];
-    int past = node;
-    while(cur != past) {
-        past = cur;
-        cur = parent[cur];
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            castle[mp[i][j]] ++;
+        }
     }
 
-    return parent[node] = cur;
+    for(int i=1;i<=p;i++) {
+        cout << castle[i] << " ";
+    }
 }
 
-void merge(edge e1) {
-    int num1 = find(e1.p1.num);
-    int num2 = find(e1.p2.num);
+void bfs(int player) {
+    while(!territory[player].empty()) {
+        int x = territory[player].front().first, y = territory[player].front().second;
+        if(visited[x][y] > s[player]) break;
 
-    if(num1 != num2) {
-        if(rnk[num1] < rnk[num2]) {
-            parent[num1] = num2;
-        } else if(rnk[num1] > rnk[num2]) {
-            parent[num2] = num1;
-        } else {
-            parent[num1] = num2;
-            rnk[num2]++;
+        territory[player].pop();
+
+        for(int i=0;i<4;i++) {
+            int mx = x + dx[i], my = y + dy[i];
+            if(mx < 0 || my < 0 || mx >= n || my >= m) continue;
+            if(mp[mx][my] != 0 || visited[mx][my] != 0) continue;
+
+            visited[mx][my] = visited[x][y] + 1;
+            mp[mx][my] = player;
+            territory[player].push({mx, my});
         }
-
-        ans += e1.weight;
-        cnt ++;
     }
 }
 
@@ -91,48 +74,34 @@ int main() {
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> n;
-    for(int i=0;i<n;i++) {
-        int x, y, z;
-        cin >> x >> y >> z;
-
-        p.push_back({i, x, y, z});
+    cin >> n >> m >> p;
+    for(int i=1;i<=p;i++) {
+        cin >> s[i];
     }
-
-    sort(p.begin(), p.end(), compX);
-
-    for(int i=0;i<n-1;i++) {
-        int dis = distance(p[i], p[i+1]);
-        e.push_back({p[i], p[i+1], dis});
-    }
-
-    sort(p.begin(), p.end(), compY);
-
-    for(int i=0;i<n-1;i++) {
-        int dis = distance(p[i], p[i+1]);
-        e.push_back({p[i], p[i+1], dis});
-    }
-
-    sort(p.begin(), p.end(), compZ);
-
-    for(int i=0;i<n-1;i++) {
-        int dis = distance(p[i], p[i+1]);
-        e.push_back({p[i], p[i+1], dis});
-    }
-
-    sort(e.begin(), e.end(), compDis);
 
     for(int i=0;i<n;i++) {
-        parent[i] = i;
-        rnk[i] = 1;
+        cin >> in;
+        for(int j=0;j<m;j++) {
+            mp[i][j] = (in[j] == '.' ? 0 : (in[j] == '#' ? -1 : in[j] - '0'));
+            if(mp[i][j] > 0) {
+                territory[mp[i][j]].push({i, j});
+            }
+        }
     }
 
-    for(int i=0;i<e.size();i++) {
-        merge(e[i]);
-        if(cnt == n) break;
+    while(true) {
+        memset(visited, 0, sizeof(visited));
+
+        if(isEnd()) {
+            break;
+        }
+
+        for(int player=1;player<=p;player++) {
+            bfs(player);
+        }
     }
 
-    cout << ans;
+    printCastle();
 
     return 0;
 }
