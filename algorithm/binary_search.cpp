191b522c0e97afa1bdeffdafe519619673d526d2
marble <ljiho1998@naver.com>
2021-03-02 20:07:10 +0900

    Binary Search Algorithm

---
+++ algorithm/binary_search.cpp
@@ -0,0 +1,21 @@
+//
+// Created by 이지호 on 21. 3. 1..
+//
+
+#include <cstdlib>
+#include <cstdio>
+
+
+int binary_search(int arr[], int value, int start, int end) {
+    int mid = (start + end) / 2;
+    if(arr[mid] == value) return mid;
+
+    if(arr[mid] > value) return binary_search(arr, value, start, mid-1);
+    return binary_search(arr, value, mid+1, end);
+}
+
+int main() {
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    printf("%d\n", binary_search(arr, 10, 0, 9));
+    printf("%d\n", binary_search(arr, 5, 0, 9));
+}
